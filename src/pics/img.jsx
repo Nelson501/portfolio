304b052ceb8f22f706e4995b140eede28fc23b59
Nelson501 <nelsonlere@gmail.com>
2025-04-09 11:33:21 +0100

    initial commit

--- src/pics/img.jsx
+++ src/pics/img.jsx
@@ -1,9 +1,6 @@
-import { Form } from "react-router-dom";
-import { FormUpdate } from "../pages/Portfolio/formUpdate";
 
 export const IMAGES = {
     WebImage : "./port-pics/myWeb.png",
-    // LoveQuote: "./logo/.png",
     computerdoc : "./port-pics/doc.png",
     FormUpdate : "./port-pics/FormUpdate.png",
     FoodRecipe: "./port-pics/foodRecipe.png",
